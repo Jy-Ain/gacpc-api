7c5b59013cb7e25e4349f71a350b6d0561e62dc3
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2025-08-19 03:51:16 +0300

    feat (linter): add linter and prettier

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,18 @@ import { AppModule } from './module/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(3000);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error('Erreur lors du démarrage de l’application :', err);
+  process.exit(1);
+});
